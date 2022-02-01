9fa03c445a2ff6658c2df22d9fc99a59a8d8d415
harsha-karanki <harsha.karanki@gmail.com>
2022-02-02 02:48:42 +0530

    Find Leap Year

---
+++ Leap year/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Leapyear
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.Write("Enter the Year :");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+                Console.WriteLine("{0} is Leap Year", year);
+            else
+                Console.WriteLine("{0} is not a Leap Year", year);
+
+
+            Console.ReadLine();
+        }
+    }
+}
