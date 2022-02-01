f7adc335275edac3752c96f63562629db9338436
harsha-karanki <harsha.karanki@gmail.com>
2022-02-02 02:34:37 +0530

    Counting the total letters in text

---
+++ Counting-program/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Counting
+{
+    class program
+
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter string :");
+        string myString = Console.ReadLine();
+        int count = 0;
+
+        foreach (char item in myString)
+        {
+           
+            if (!char.IsWhiteSpace(item))
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Total letters: " + count);
+        Console.ReadLine();
+    }
+}
+}
